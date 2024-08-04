bad0286ed2c2c8faa03c78c4e888290de8d48b20
Ruchi Jha <ruchijha139@gmail.com>
2024-08-04 20:11:53 +0530

    output values
    event emitter for the selected user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OutletContext } from '@angular/router';
 
 
 @Component({
@@ -12,6 +13,10 @@ export class UserComponent {
 
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Input({required: true}) id!: string;
+
+  @Output() select = new EventEmitter<string>();
+
 
   get imagePath(){
     return 'assets/users/' +this.avatar;
@@ -19,6 +24,7 @@ export class UserComponent {
 
 
   onSelectUser(){
+    this.select.emit(this.id);
   }
 
 }
