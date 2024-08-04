b85833b0ea754653e4c4e8c5b2188dc5e4679b5c
Ruchi Jha <ruchijha139@gmail.com>
2024-08-04 15:11:59 +0530

    adding inputs
    defining custom events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -12,15 +10,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomIndex]
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
   get imagePath(){
-    return 'assets/users/'+ this.selectedUser.avatar;
+    return 'assets/users/' +this.avatar;
   }
 
+
   onSelectUser(){
-    const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 
 }
