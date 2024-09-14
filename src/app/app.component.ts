d7a684826703de99217c6d58a34c0c82839f2c56
Ruchi Jha <ruchijha139@gmail.com>
2024-09-15 00:02:09 +0530

    user specific task to be outputted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
