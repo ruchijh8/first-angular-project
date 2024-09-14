dbb3bc968fc3253678d40acf2874fd40a2f90a11
Ruchi Jha <ruchijha139@gmail.com>
2024-09-14 18:26:13 +0530

    accept object as input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,20 +11,22 @@ import { OutletContext } from '@angular/router';
 })
 export class UserComponent {
 
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  @Input({required: true}) id!: string;
+  @Input() user!: {
+    avatar : string;
+    name: string;
+    id: string;
+  };
 
   @Output() select = new EventEmitter<string>();
 
 
   get imagePath(){
-    return 'assets/users/' +this.avatar;
+    return 'assets/users/' +this.user.avatar;
   }
 
 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
 }
