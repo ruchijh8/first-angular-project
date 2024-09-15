f49faa3e156f6277d0f88280debb13c5c56cd148
Ruchi Jha <ruchijha139@gmail.com>
2024-09-15 08:52:48 +0530

    Outputting Task Data in Task comp

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task{
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
@@ -9,4 +17,5 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
 
+@Input({required: true}) task!: Task;
 }
